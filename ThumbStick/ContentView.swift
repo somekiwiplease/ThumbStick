4351e3a84dee199ca239390267c1d2637208d6fa
Shar <somekiwipls@gmail.com>
2026-07-03 13:52:02 -0700

    Initial Commit

---
+++ ThumbStick/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ThumbStick
+//
+//  Created by Sharvari Karkibylu Suresh on 7/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ThumbStick/ThumbStickApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThumbStickApp.swift
+//  ThumbStick
+//
+//  Created by Sharvari Karkibylu Suresh on 7/3/26.
+//
+
+import SwiftUI
+
+@main
+struct ThumbStickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
